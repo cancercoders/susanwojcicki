9b319356b77c3abe5df124d54701a43f9f3002ba
Vlad Nistor <30766091+cartridgetilt@users.noreply.github.com>
2018-05-27 18:25:38 +1200

    update
    shuts down computer correctly now

--- text.bat
+++ text.bat
@@ -3,4 +3,4 @@
 echo Windows Defender: We have spotted a virus and we will shutdown the computer for safety reasons
 echo You have a minute to save your files
 timeout 60 /nobreak
-shutdown -s
+shutdown.exe /s /t 00
